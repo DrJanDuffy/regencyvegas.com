515ff8c0d16255be7808b7f690aa9387246dbd5e
genekellyboyle <DrDuffy@bhhsnv.com>
2025-12-18 06:20:48 -0800

    Add RealScout home value widget globally

--- client-site/src/Layout/Mainlayout.jsx
+++ client-site/src/Layout/Mainlayout.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Navbar from '../pages/Shared/Navbar/Navbar';
 import { Outlet } from 'react-router-dom';
 import RealScoutSearch from '../pages/Shared/RealScout/RealScoutSearch';
+import RealScoutHomeValue from '../pages/Shared/RealScout/RealScoutHomeValue';
 import RealScoutOfficeListings from '../pages/Shared/RealScout/RealScoutOfficeListings';
 
 const Mainlayout = () => {
@@ -10,6 +11,7 @@ const Mainlayout = () => {
             <Navbar />
             <RealScoutSearch />
             <Outlet />
+            <RealScoutHomeValue />
             <RealScoutOfficeListings />
         </div>
     );
