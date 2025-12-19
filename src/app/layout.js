559688a82f5e10187e76c622b16270cb6704b5d1
genekellyboyle <DrDuffy@bhhsnv.com>
2025-12-19 09:08:45 -0800

    Add Google Tag Manager (GTM-WBDMTNL4) to root layout
    - Add GTM script in head section for all pages
    
    - Add GTM noscript fallback immediately after body tag
    
    - GTM will track across entire site automatically

--- src/app/layout.js
+++ src/app/layout.js
@@ -24,6 +24,17 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en" className={`${inter.variable} ${playfair.variable}`}>
       <head>
+        {/* Google Tag Manager */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-WBDMTNL4');`,
+          }}
+        />
+        {/* End Google Tag Manager */}
         {/* Preconnect to external resources for faster loading */}
         <link rel="preconnect" href="https://em.realscout.com" crossOrigin="" />
         <link rel="preconnect" href="https://www.googletagmanager.com" crossOrigin="" />
@@ -38,6 +49,16 @@ export default function RootLayout({ children }) {
         />
       </head>
       <body className="font-sans antialiased">
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-WBDMTNL4"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
         {/* Google Analytics */}
         <Script
           src="https://www.googletagmanager.com/gtag/js?id=G-L03F985VF0"
